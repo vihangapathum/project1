3f830626954cee7cd20a904ed40e0f13eba4759d
vihangapathum <vihangapathum968@gmail.com>
2024-12-23 03:19:58 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+# Get input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum
+result = num1 + num2
+
+# Display the result
+print("The sum of", num1, "and", num2, "is:", result)
